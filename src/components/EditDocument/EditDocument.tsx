ba32cba6149e56c36a64c1e7dec56ae6dda632c5
jsaji06 <the.joshua.saji@gmail.com>
2025-07-23 20:05:12 -0400

    fixed fetch

--- src/components/EditDocument/EditDocument.tsx
+++ src/components/EditDocument/EditDocument.tsx
@@ -102,8 +102,7 @@ function EditDocument() {
       setLoadingPanel(true);
       setAiPanelActive(true);
 
-      // fetch("https://eloquence-68ro.onrender.com/get_points", {
-      fetch("http://localhost:8000/get_points", {
+      fetch("https://eloquence-68ro.onrender.com/get_points", {
         method: "POST",
         body: JSON.stringify({
           writing: writing ?? text,
