bbadea3dd7caebe8adb209e44e280c0a293d19d5
jsaji06 <the.joshua.saji@gmail.com>
2025-07-30 16:03:25 -0400

    fixed styling

--- src/components/BestExperience/BestExperience.tsx
+++ src/components/BestExperience/BestExperience.tsx
@@ -3,7 +3,7 @@
 export default function () {
     return (
         <>            
-            <div className="login" style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+            <div style={{width:"100vw", height:"100vh", display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
                 <h1>Best Experienced on Desktop</h1>
                 <p>To make the most of this app, we strongly recommend using this app on a laptop, desktop, or PC.</p>
             </div>
