c0b0238f57a6c9ce34938b88541c4e3f14387dab
jsaji06 <the.joshua.saji@gmail.com>
2025-07-30 16:04:04 -0400

    fixed styling

--- src/components/BestExperience/BestExperience.tsx
+++ src/components/BestExperience/BestExperience.tsx
@@ -4,8 +4,8 @@ export default function () {
     return (
         <>            
             <div style={{width:"100vw", height:"100vh", display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
-                <h1>Best Experienced on Desktop</h1>
-                <p>To make the most of this app, we strongly recommend using this app on a laptop, desktop, or PC.</p>
+                <h1 style={{textAlign:"center"}}>Best Experienced on Desktop</h1>
+                <p style={{textAlign:"center"}}>To make the most of this app, we strongly recommend using this app on a laptop, desktop, or PC.</p>
             </div>
         </>
     )
